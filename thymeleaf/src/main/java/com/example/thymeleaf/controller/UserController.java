9c4c577fefb6f6eb2a4357b91bcd808a73c188ce
Jimin Kim <0305jimin@gmail.com>
2025-10-20 14:12:49 +0900

    docs: adding annotations

--- thymeleaf/src/main/java/com/example/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/UserController.java
@@ -11,6 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+/*
+요청이 들어오면 Dispatcher Servlet은 Handler Mapping에게 대응되는 컨트롤러가 있는지 물어본다.
+있으면 컨트롤러 정보를 반환해준다.
+Dispatcher Servlet은 이를 Handler Adapter에 전달하고 Handler Adapter는 컨트롤러를 호출한다.
+
+호출된 Controller는 필요 시 Model에게 데이터를 요청해 응답받아 사용하며 작업을 수행한다.
+수행 후 View Resolver에게 뷰 이름을 반환하면 대응되는 뷰에 모델을 반영해 동적 처리를 완료한다.
+
+ */
 @Controller
 public class UserController {
     private final UserService userService;
@@ -29,6 +38,9 @@ public class UserController {
     @GetMapping("/login")
     public String loginForm() {
         return "login";
+        /* 뷰 이름을 반환하면 View Resolver는
+           classpath:/templates/뷰이름/.html 파일을 찾아 렌더링한다.
+         */
     }
 
     // 로그인 처리 (POST)
